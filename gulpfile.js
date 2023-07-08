3b9d27a0845985b1136dcef57abaf611bb51489c
pagemaker06 <orazali2017@gmail.com>
2023-07-08 22:41:12 +0600

    added header, added fonts and remade gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,7 @@ const uglify = require('gulp-uglify-es').default;
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
 const clean = require('gulp-clean');
-const avif = require('gulp-avif');
-const webp = require('gulp-webp');
 const imagemin = require('gulp-imagemin');
-const newer = require('gulp-newer');
 const fonter = require('gulp-fonter');
 const ttf2woff2 = require('gulp-ttf2woff2');
 
@@ -23,22 +20,19 @@ function fonts() {
 }
 
 function images() {
-  return src([
-    'app/images/src/*.*',
-    '!app/images/src/*.svg'
-  ])
-    .pipe(newer('app/images'))
-    .pipe(avif({ quality: 50 }))
-
-    .pipe(src('app/images/src/*.*'))
-    .pipe(newer('app/images'))
-    .pipe(webp())
-
-    .pipe(src('app/images/src/*.*'))
-    .pipe(newer('app/images'))
-    .pipe(imagemin())
-
-    .pipe(dest('app/images'))
+  return src('app/images/**/*.*')
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.mozjpeg({ quality: 75, progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
+    ]))
+    .pipe(dest('dist/images'))
 }
 
 function scripts() {
@@ -72,7 +66,7 @@ function watching() {
     notify: false
   })
   watch(['app/scss/*.scss'], styles)
-  watch(['app/images/src'], images)
+  watch(['app/images'], images)
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
   watch(['app/*.html']).on('change', browserSync.reload)
 }
@@ -89,7 +83,6 @@ function building() {
     'app/fonts/*.*',
     'app/js/main.min.js',
     'app/**/*.html',
-    '!app/images/**/*.html'
   ], { base: 'app' })
     .pipe(dest('dist'))
 }

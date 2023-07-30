4ecdbc5adbdd66ca017b61d8dcd164ffa10b85dc
pagemaker06 <orazali2017@gmail.com>
2023-07-30 16:11:50 +0600

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,32 @@
 $(function () {
+
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+  $('.button-list').on('click', function () {
+    $('.product-item').addClass('product-item--list');
+    $('.shop-content__inner').addClass('shop-content__inner--list');
+  });
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list');
+    $('.shop-content__inner').removeClass('shop-content__inner--list');
+  });
+
+  $('.shop-content__filter-sort').styler();
+
+  $('.filter-price__input').ionRangeSlider({
+    type: 'double',
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    }
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
